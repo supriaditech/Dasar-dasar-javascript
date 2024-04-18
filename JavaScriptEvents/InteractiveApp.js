4ffe0923b318d5750d808f1957c619f1102224a0
supriadiii <supriadiadi1001@gmail.com>
2024-04-18 16:56:50 +0700

    update

---
+++ JavaScriptEvents/InteractiveApp.js
@@ -0,0 +1,8 @@
+// Menampilkan teks ke console
+console.log("ini adalah contoh penggunaan console");
+
+// Menampilkan pesan alert kepada pengguna
+alert("ini contoh penggunaan alert");
+
+// Meminta input dari pengguna dalam bentuk prompt
+prompt("ini adalah contoh prompt");
--- Method/MathAndRandom.js
+++ Method/MathAndRandom.js
@@ -1,8 +1,8 @@
-console.log(Math.PI);
-console.log(Math.round(1.6));
-console.log(Math.round(1.4));
-console.log(Math.ceil(1.2));
-console.log(Math.floor(1.8));
-console.log(Math.min(1, 2, 3, 4, 5));
-console.log(Math.max(1, 2, 3, 4, 5));
-console.log(Math.random());
+console.log(Math.PI); // Menampilkan nilai PI (~3.14)
+console.log(Math.round(1.6)); // Membulatkan angka ke nilai terdekat, dalam hal ini menjadi 2
+console.log(Math.round(1.4)); // Membulatkan angka ke nilai terdekat, dalam hal ini menjadi 1
+console.log(Math.ceil(1.2)); // Membulatkan angka ke atas, menjadi 2
+console.log(Math.floor(1.8)); // Membulatkan angka ke bawah, menjadi 1
+console.log(Math.min(1, 2, 3, 4, 5)); // Menemukan nilai terkecil dari daftar angka, yaitu 1
+console.log(Math.max(1, 2, 3, 4, 5)); // Menemukan nilai terbesar dari daftar angka, yaitu 5
+console.log(Math.random()); // Menghasilkan angka acak antara 0 (inklusif) dan 1 (eksklusif)

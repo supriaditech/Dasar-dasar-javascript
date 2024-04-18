4a8eed61a28f03efa3065616b7b1d3ac193db2c8
supriadiii <supriadiadi1001@gmail.com>
2024-04-18 16:55:54 +0700

    menambahkan pemberlajaran logic dan pengenalan interactive javascript

--- Logic/IfStatement.js
+++ Logic/IfStatement.js
@@ -1,17 +1,26 @@
+// Mendeklarasikan variabel umur dengan nilai 20
 let umur = 20;
 
+// Memeriksa apakah umur lebih besar atau sama dengan 18
 if (umur >= 18) {
+  // Jika benar, mencetak "Anda sudah dewasa"
   console.log("Anda sudah dewasa");
 } else {
+  // Jika salah, mencetak "Anda masih muda"
   console.log("Anda masih muda");
 }
 
+// Mendeklarasikan variabel angka dengan nilai acak antara 0 dan 1
 let angka = Math.random();
+
+// Mencetak nilai dari variabel angka
 console.log("Ini adalah nilai dari angka", angka);
 
-// kondisi pengecekan
+// Memeriksa apakah angka lebih besar atau sama dengan 1
 if (angka >= 1) {
+  // Jika benar, mencetak "angka lebih besar dari sama dengan 1"
   console.log("angka lebih besar dari sama dengan 1");
 } else {
+  // Jika salah, mencetak "angka kurang dari sama dengan 1"
   console.log("angka kurang dari sama dengan 1");
 }
